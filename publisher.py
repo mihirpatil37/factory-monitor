82a21474937a8cd6498824a26b04af4cb9defda1
mihirpatil37 <patilmihir9588@gmail.com>
2026-04-08 19:36:37 +0200

    simulate temperature, pressure and machine status

--- publisher.py
+++ publisher.py
@@ -1,1 +1,14 @@
-print("Hello Factory")
+import time
+import random
+
+while True:
+    temperature = round(random.uniform(60.0, 90.0), 2)
+    pressure = round(random.uniform(1.0, 5.0), 2)
+    status = random.choice(["RUNNING", "STOPPED"])
+
+    print(f"Temperature : {temperature} °C")
+    print(f"Pressure    : {pressure} bar")
+    print(f"Status      : {status}")
+    print("---")
+
+    time.sleep(2)
